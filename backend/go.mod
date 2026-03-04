19b8e09a39887607e761250afaf27717b5531654
dmslmvsk <dmslmvsk@gmail.com>
2026-03-04 22:08:34 +0100

    feat: implement password hashing and user registration handler

--- backend/go.mod
+++ backend/go.mod
@@ -5,4 +5,5 @@ go 1.26
 require (
 	github.com/golang-migrate/migrate/v4 v4.19.1 // indirect
 	github.com/lib/pq v1.11.2 // indirect
+	golang.org/x/crypto v0.48.0 // indirect
 )
--- backend/internal/api/user_handler.go
+++ backend/internal/api/user_handler.go
@@ -1,7 +1,12 @@
 package api
 
 import (
+	"encoding/json"
+	"net/http"
 	"time"
+
+	"github.com/dmslmvsk/daily-tracker/backend/internal/repository"
+	"github.com/dmslmvsk/daily-tracker/backend/internal/util"
 )
 type CreateUserRequest struct {
 	Email    string `json:"email"`
@@ -12,4 +17,44 @@ type CreateUserResponse struct {
 	ID int32 `json:"id"`
 	Email string `json:"email"`
 	CreatedAt time.Time `json:"created_at"`
+}
+
+type UserHandler struct {
+	store *repository.Queries
+}
+
+func NewUserHandler(store *repository.Queries) *UserHandler{
+	return &UserHandler{store:store}
+}
+
+func (h* UserHandler) CreateUser(w http.ResponseWriter, r *http.Request){
+	var req CreateUserRequest
+	err := json.NewDecoder(r.Body).Decode(&req)
+	if err != nil{
+		http.Error(w,err.Error(),http.StatusBadRequest)
+		return
+	}
+	hashedPassword,err := util.HashPassword(req.Password)
+	if err != nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+
+	arg:= repository.CreateUserParams{
+		Email: req.Email,
+		PasswordHash: hashedPassword,
+	}
+	user,err := h.store.CreateUser(r.Context(),arg)
+	if err != nil{
+		http.Error(w,err.Error(),http.StatusInternalServerError)
+		return
+	}
+	resp:=CreateUserResponse{
+		ID: user.ID,
+		Email: user.Email,
+		CreatedAt: user.CreatedAt,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(resp)
 }
